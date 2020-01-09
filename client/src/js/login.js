757781920089af423a74da4282b828a833238904
ms-uvm <marc.spree@speisekarte.de>
2020-01-09 16:17:04 +0100

    try

--- client/src/js/login.js
+++ client/src/js/login.js
@@ -39,23 +39,22 @@ export default class Login extends React.Component {
 				<h1 className="text-center pt-5"><u>Welcome to MarcsBlog</u></h1>
 				<div className="card position-absolute border p-3 bg-dark login text-center mt-auto mb-auto">
 					<form id="formLogin" role="form" className="form-signin" action="http://192.168.50.2/marcsblog/controller/login.php"
-						encType="multipart/form-data" method="post">
+						  encType="multipart/form-data" method="post">
 						<div className="form-group">
 							<label className="label-small">Email</label>
 							<div>
-								<input type="text" className="form-control" name="email" required autoFocus
-									value={this.state.email} onChange={this.handleChange}/>
+								<input type="text" className="form-control ASD" name="email" required autoFocus
+									   value={this.state.email} onChange={this.handleChange}/>
 							</div>
 							{errorNumber === 2 ?
-
-								<div className="text-danger p-2">{this.props.errorText}</div>
+								<div className="text-danger p-2 kkk">{this.props.errorText}</div>
 								: null}
 						</div>
-						<div className="form-group">
+						<div className="form-group dsfajk">
 							<label className="label-small">Enter pw!</label>
 							<div>
 								<input type="password" className="form-control pw-form" name="password" required
-									value={this.state.password} onChange={this.handleChange}/>
+									   value={this.state.password} onChange={this.handleChange}/>
 							</div>
 							{errorNumber === 1 ?
 								<div className="text-danger p-2">{this.props.errorText}</div>
@@ -63,7 +62,7 @@ export default class Login extends React.Component {
 						</div>
 						<div className="form-group">
 							<button onClick={this.handleSubmit} className="login-btn round-border btn btn-success"
-								type="submit">Log in !
+									type="submit">Log in !
 							</button>
 						</div>
 						{errorNumber === 5 ?
