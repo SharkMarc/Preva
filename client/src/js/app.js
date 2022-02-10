bcaedd12a4b8e969ab0730693c2e61ae4169a7af
Marc Spree <marc.spree@speisekarte.de>
2022-02-10 09:21:14 +0100

    fixed responsive design for tabs

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -131,8 +131,8 @@ class App extends React.Component {
 					method:  'POST',
 					headers: {
 						'Content-Type': 'application/json',
-//						"Access-Control-Allow-Origin": "*",
-//						"Access-Control-Allow-Methods": "*"
+						"Access-Control-Allow-Origin": "*",
+						"Access-Control-Allow-Methods": "*"
 					},
 					body:    JSON.stringify(data),
 					cache:   'no-cache'
